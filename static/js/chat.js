6c4833a9562a870bd32565dddb11c6e035a7cf9c
JeremySu0818 <xinghong.su0818@gmail.com>
2026-01-07 05:18:35 +0800

    refactor: update API request URLs

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,4 +1,4 @@
-const API_URL = "https://gemini-api.zssdmrofficial.workers.dev/"; 
+const API_URL = "https://gemini-api.zssdmr.dpdns.org/";
 const CHAT_STATE_KEY = 'chat_state_v1';
 
 function isHardReload() {
